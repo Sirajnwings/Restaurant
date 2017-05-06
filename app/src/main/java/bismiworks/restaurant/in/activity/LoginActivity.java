5c0975cce4a45218bb12be1cc87d6e9584fa0d22
NSEIPL25 <nseipl25@nanosoft.com>
2017-05-06 16:23:25 +0530

    Initial commit

---
+++ app/src/main/java/bismiworks/restaurant/in/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package bismiworks.restaurant.in.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import bismiworks.restaurant.in.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
